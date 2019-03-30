86297edb9cb01c191e1dfc447504b8c6f440a975
Rickard Laurin <rickard.laurin@gmail.com>
2019-03-30 17:40:29 +0100

    fix: create a new client instance each request

--- packages/spotify-client/index.ts
+++ packages/spotify-client/index.ts
@@ -1,11 +1,11 @@
 import SpotifyWebApi from 'spotify-web-api-node'
 
-const spotifyApi = new SpotifyWebApi({
-  clientId: process.env.SPOTIFY_CLIENT_ID,
-  clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-})
-
 export default async () => {
+  const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.SPOTIFY_CLIENT_ID,
+    clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+  })
+
   const { body: token } = await spotifyApi.clientCredentialsGrant()
 
   spotifyApi.setAccessToken(token.access_token)
