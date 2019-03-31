1b24341ce85f44cd5d89c995d0173e235c1810d0
Rickard Laurin <rickard.laurin@gmail.com>
2019-03-31 11:48:57 +0200

    test: add tests to spotify-utils

---
+++ packages/spotify-utils/__tests__/index.spec.ts
@@ -0,0 +1,50 @@
+import { createTrack, queryParam } from '../'
+
+describe('#createTrack', () => {
+  test('should parse a spotify track into an easier structure', () => {
+    const track = {
+      album: {
+        images: [
+          {
+            url: 'first.jpg',
+          },
+          {
+            url: 'second.jpg',
+          },
+        ],
+        name: '30 Seconds to Mars',
+        release_date: '2000-01-01',
+      },
+      artists: [{ name: '30 Seconds to Mars' }],
+      duration_ms: 100000,
+      id: '1337',
+      name: 'Echelon',
+      uri: 'spotify:track:1337',
+    }
+
+    expect(createTrack(track)).toMatchInlineSnapshot(`
+Object {
+  "albumName": "30 Seconds to Mars",
+  "artist": "30 Seconds to Mars",
+  "cover": "second.jpg",
+  "duration": 100000,
+  "id": "1337",
+  "name": "Echelon",
+  "releaseDate": "2000-01-01",
+  "uri": "spotify:track:1337",
+}
+`)
+  })
+})
+
+describe('#queryParam', () => {
+  test('returns a query param', () => {
+    expect(queryParam({ path: '/?q=test', param: 'q' })).toEqual('test')
+  })
+
+  test('throws an error if param does not exist', () => {
+    expect(() => queryParam({ path: '/?t=test', param: 'q' })).toThrowError(
+      'No "q" provided'
+    )
+  })
+})
